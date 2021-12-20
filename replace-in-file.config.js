cb17fba2e0c1496fbf979c9206e2a2c1765c9379
Vitaly Pinchuk <vitaly.pinchuk@masterdata.ru>
2021-12-20 15:45:24 +0300

    change cli from push-dir to gh-pages

--- replace-in-file.config.js
+++ replace-in-file.config.js
@@ -1,12 +1,13 @@
 const pjson = require('./package.json');
 const { devDependencies } = pjson;
+const ghPagesModule = { name: 'gh-pages' };
 
 // TODO: need mapping [multiple keys](https://github.com/adamreisnz/replace-in-file#multiple-values-with-different-replacements)
-const verPushDir = devDependencies['push-dir'];
+ghPagesModule.ver = devDependencies[ghPagesModule.name];
 
 const options = {
-	from: '@@push-dir',
-	to: verPushDir,
+	from: '@@ghPagesModule',
+	to: `${ghPagesModule.name}@${ghPagesModule.ver}`,
 };
 
 module.exports = options;
