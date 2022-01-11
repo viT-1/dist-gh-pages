8ccbffd4531564dd5332e2d8e1ec3238a5e81a49
Vitaly Pinchuk <vitaly.pinchuk@masterdata.ru>
2022-01-11 16:06:44 +0300

    cutout custom esm resolving (index & extensions) preferred to use tsc-alias which generates relative paths from aliases. Also no need to register aliases for node, because of dist folder using (otherwise should use tsconfig-paths/register).

--- readme.md
+++ readme.md
@@ -2,7 +2,10 @@ Simplest project for hosting static gh-pages (SPA further).
 Use `npm run deploy` for deploying dist folder to remote (no need to commit) repository on GitHub,
 then you can see [demo site](https://vit-1.github.io/dist-gh-pages/).
 
-ESM aliases resolved by hands with [replace-in-file](https://github.com/adamreisnz/replace-in-file) node [script](https://github.com/viT-1/dist-gh-pages/blob/dce502b314fb81b4bf086af6b64fe983ff6cdde9/package.json#L21).
+ESM aliases resolved by [tsc-alias](https://github.com/justkey007/tsc-alias) which generates relative paths
+without [MS tsc](https://github.com/microsoft/TypeScript/tree/main/bin) or [ttsc](https://github.com/cevek/ttypescript) transpilers.
+Node [script](https://github.com/viT-1/dist-gh-pages/blob/main/replace-in-file.js) which use [replace-in-file](https://github.com/adamreisnz/replace-in-file)
+only for custom replacing in templates (no need for custom import resolving with index.js files or file extensions).
 
 This project is not using TypeScript but using tsconfig.json > paths
 to have single paths configuration for all (eslint, node, jest)!
--- replace-in-file.js
+++ replace-in-file.js
@@ -1,8 +1,7 @@
 import replace from 'replace-in-file';
-import aliasConfig from './tsconfig.json';
 import pjson from './package.json';
 
-const { dependencies, name: ghPagesFolder } = pjson;
+const { dependencies } = pjson;
 
 const ghPagesModule = { name: 'gh-pages' };
 ghPagesModule.ver = dependencies[ghPagesModule.name];
@@ -12,34 +11,4 @@ replace({
 	files: './dist/index.html',
 	from: '@@ghPagesModule',
 	to: `${ghPagesModule.name}@${ghPagesModule.ver}`,
-});
-
-// TODO: configuration for "#" or "@" symbol?
-// uses alias with #
-let jsStrFrom = new Array(
-	/\s?\/{2}\s(.*)/gm, // remove comments which can use ".js" text, breaking regexp for suffixes
-	/^(.* from )('|")(?!.*(\.js|#))(.*)('|")/gm, // import not aliased esm without extensions (but not json)
-	/^(.* from )('|")(#)(?!.*(\.js))(.*)('|")/gm, // import aliased esm without extensions (but not json)
-);
-
-let jsStrTo = ['', '$1$2$4.js$2', '$1$2$3$4$5/index.js$2'];
-
-// TODO: instead regex replacing alias map (bad replace '/*'), use tsconfig-paths createMatchPath
-// use eslint configuration for aliases
-const aliasMap = aliasConfig.compilerOptions.paths;
-Object.keys(aliasMap).forEach(key => {
-	jsStrFrom.push(key.replace('/*', ''));
-	jsStrTo.push(
-		// bad code, inner knowledge, wwwroot is viT-1.io not this project root!
-		aliasMap[key][0].replace('/*', '').replace(
-			'./src/',
-			`/${ghPagesFolder}/`,
-		));
-});
-
-// replace imports in esm
-replace({
-	files: './dist/**/*.js',
-	from: jsStrFrom,
-	to: jsStrTo,
 });
